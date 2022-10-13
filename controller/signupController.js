fbc550cc177e7daa60b12989bf6c9a7e35e4ec8c
unknown <sandesh.prabhu@robosoftin.com>
2022-10-13 09:55:18 +0530

    initial commit

--- controller/signupController.js
+++ controller/signupController.js
@@ -1,8 +1,12 @@
 const model=require('../models/user')
 const bcrypt = require('bcrypt')
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
 
 async function signup(req,res){
-    const hashedPassword = await bcrypt.hash(req.body.MPin.toString(), 10)
+    if(req.body.MPin.toString().length!=4) return res.send("Enter a valid 4-digit MPin")
+    const hashedPassword = await bcrypt.hash(req.body.MPin.toString(), process.env.SALT)
     const user = new model({ mobileNumber: req.body.mobileNumber, MPin: hashedPassword })
 //     let {mobileValidation, MPinValidation} = validation(user)
 //     if(mobileValidation==true && MPinValidation==true){
@@ -18,7 +22,7 @@ async function signup(req,res){
 //     }
     await user.save((err)=>{
         if(err) res.send(err)
-        else res.send('ok')})
+        else res.send('Congrats!!! Success\nSignin to access the vault')})
 }
 
 module.exports=signup
