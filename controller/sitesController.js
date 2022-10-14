54bd1e078d3f4dc752609e20c78387c9f0369b77
unknown <sandesh.prabhu@robosoftin.com>
2022-10-14 10:50:25 +0530

    password

---
+++ controller/sitesController.js
@@ -0,0 +1,82 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
+const Cryptr = require('cryptr');
+const cryptr = new Cryptr(process.env.PASSWORD_KEY)
+const siteModel=require('../models/sites')
+
+async function addSite(req,res){
+    const site = new siteModel({
+        mobileNumber:req.user.mobileNumber,
+        URL: req.body.URL,
+        siteName: req.body.siteName,
+        folder:req.body.folder,
+        userName:req.body.userName,
+        password: await cryptr.encrypt(req.body.password),
+        notes:abc()
+    })
+
+    site.save((err)=>{
+        if(err) return res.send(err)
+        else {
+            res.send('Saved Successfully')
+    
+        }})
+}
+
+async function home(req,res){
+        let folder = req.body.folder
+        await siteModel.find({$and:[{folder:folder},{mobileNumber:req.user.mobileNumber}]},{__v:0,_id:0,mobileNumber:0}/*projection*/,function (err, documents)/*callback*/ {
+            if (err)  return res.sendStatus(401).send(err)
+            else{
+                if(documents.length==0){
+                    return res.send(`No sites in ${folder} category!`)
+                }
+                documents.map(docs=>docs.password= cryptr.decrypt(docs.password))
+                return res.send(documents)
+            } }).clone()
+}
+
+async function search(req,res){
+    // let search=req.query.search
+
+    // var regex = new RegExp(search, 'i');  // 'i' makes it case insensitive
+
+    // await siteModel.find({$and:[{$search:{$text: /Sandesh/i}},{mobileNumber:req.user.mobileNumber}]}, function(err,data){
+    //     if(err) return res.send(err)
+    //     return res.send(data);
+    // }).clone();
+}
+
+
+
+
+
+async function selectedSite(req,res){   
+    
+    // opening selected site details using siteName along with userName
+    // await siteModel.find({$and:[{mobileNumber:req.user.mobileNumber},{siteName:req.body.siteName},{userName:req.body.userName}]},{__v:0,_id:0,mobileNumber:0}/*projection*/,function (err, documents)/*callback*/ {
+    
+    // opening selected site details using the _id received by post method
+    await siteModel.find({_id:req.body._id},{__v:0,_id:0,mobileNumber:0}/*projection*/,function (err, documents)/*callback*/ {
+            if (err)  return res.sendStatus(401).send(err)
+        else {
+            documents.map(docs=>docs.password= cryptr.decrypt(docs.password))
+            return res.send(documents)
+        }}).clone()
+}
+
+async function editSite(req,res){
+    const docs = await siteModel.find({_id:req.body._id},{__v:0,mobileNumber:0}/*projection*/,function (err)/*callback*/ {
+        if (err)  return res.sendStatus(401).send(err)}).clone()
+    delete req.body._id;    //remove _id from body such that _id won't get updated
+    if(req.body.password) {req.body.password = await cryptr.encrypt(req.body.password)} //encryt password
+    const data=await siteModel.findByIdAndUpdate({_id:docs[0]._id},req.body,function(err){
+            if(err) console.log(err)}).clone()
+    data.password= cryptr.decrypt(data.password)
+    res.send(data)
+}
+
+
+
+module.exports={addSite,home,search,selectedSite,editSite}
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express=require('express')
 const authorization=require('../middleware/authorization')
 const signup=require('../controller/signupController')
 const signin=require('../controller/signinController')
-const {addSite,home,selectedSite}=require('../controller/sitesContoller')
+const {addSite,home,search,selectedSite,editSite}=require('../controller/sitesController')
 
 
 const router=express.Router()
@@ -11,7 +11,9 @@ router.post('/signup',signup)
 router.post('/signin',signin)
 router.post('/home/addSite',authorization,addSite)
 router.post('/home',authorization,home)
+router.get('/home',authorization,search)
 router.post('/home/selectedSite',authorization,selectedSite)
+router.post('/home/selectedSite/editSite',authorization,editSite)
 
 // const model=require('../models/user')
 
