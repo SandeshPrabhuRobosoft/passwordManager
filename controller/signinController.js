c2732c37eb5b43a7635c30eca623b0aac7351aef
unknown <sandesh.prabhu@robosoftin.com>
2022-10-14 17:54:12 +0530

    final commit

--- controller/signinController.js
+++ controller/signinController.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcrypt')
 const userModel=require('../models/user')
 const jwt = require("jsonwebtoken");
+const client=require('twilio')(/*accountSid*/"accountSid",/*authToken*/"authToken")
 require('dotenv').config()
 
 async function signin(req, res) {
@@ -10,6 +11,7 @@ async function signin(req, res) {
   try {
     if(await bcrypt.compare(req.body.MPin.toString(), user.MPin)) { //compare MPin 
     const token = jwt.sign({ mobileNumber: user.mobileNumber }, process.env.JWTPRIVATEKEY, {expiresIn: "7d",}); // token signing using mobileNumber
+    // const refreshToken=jwt.sign(user,process.env.REFRESH_TOKEN_SECRET)
 		res.status(200).send({ token: token, message: "logged in successfully" }); 
     } else {
       res.sendStatus(401).send('Not Allowed')
@@ -19,4 +21,41 @@ async function signin(req, res) {
   }
 }
 
-module.exports=signin
+async function forgotPassword(req, res) {
+  const [user] = await userModel.find({mobileNumber: req.body.mobileNumber}).clone()
+  if (user == null)
+    return res.status(404).send('Cannot find user')
+  const mobileNumber=req.body.mobileNumber
+  const OTP=Math.random() * (9999 - 1000) + 1000
+  try{
+     await client.messages.create({
+      body: OTP, 
+      from: '+15017122661', 
+      to: mobileNumber.toString()
+    })
+  }
+  catch{error=> res.sendStatus(500).send(error)};
+  //compareOTP()
+  // if wrong otp return invalid
+  //else
+}
+
+
+async function refreshToken(req,res){
+let accessToken=req.headers["authorization"].split(" ")[1]
+  let refreshToken=process.env.REFRESH_TOKEN_SECRET
+  if(refreshToken==null) return res.sendStatus(401)
+  if(!refreshTokens.includes(refreshToken)) return res.sendStatus(403)
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err,user)=>{
+      if(err) return res.sendStatus(403)
+      // const accessToken=generateAccessToken({name:user.name}) // for security entire user is not sent, only username is sent
+      res.json({accessToken:accessToken})
+  })
+}
+
+async function logout(req,res){ // to logout such that no more refresh tokens can be created
+  refreshTokens=refreshTokens.filter(token=>token!==req.body.token)
+  res.sendStatus(204)
+}
+
+module.exports={signin,forgotPassword,refreshToken,logout}
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,21 @@
 const express=require('express')
 const authorization=require('../middleware/authorization')
 const signup=require('../controller/signupController')
-const signin=require('../controller/signinController')
+const {signin,forgotPassword,refreshToken,logout}=require('../controller/signinController')
 const {addSite,home,search,selectedSite,editSite}=require('../controller/sitesController')
 
 const router=express.Router()
 
 router.post('/signup',signup)
 router.post('/signin',signin)
+router.post('/refreshToken',refreshToken)
+router.delete('/logout',logout)
+router.post('/forgotPassword',forgotPassword)
 router.post('/home/addSite',authorization,addSite)
 router.post('/home',authorization,home)
 router.get('/home',authorization,search)
 router.post('/home/selectedSite',authorization,selectedSite)
 router.post('/home/selectedSite/editSite',authorization,editSite)
+router.get('/sync',authorization,home)
 
 module.exports=router
