fc472335a7d069755a452979b207aad8d48b159a
unknown <sandesh.prabhu@robosoftin.com>
2022-10-25 16:33:07 +0530

    forgot password

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,26 @@
 const express=require('express')
 const authorization=require('../middleware/authorization')
 const signup=require('../controller/signupController')
-const {signin/*,forgotPassword*/}=require('../controller/signinController')
+const {signin,generateOTP,verifyOTP}=require('../controller/signinController')
 const {addSite,home,search,selectedSite,editSite,deleteSite}=require('../controller/sitesController')
 const {GetNewAccessToken,logout}=require('../controller/refreshToken')
 const router=express.Router()
+const resetMPin=require('../controller/passwordController')
 
 router.post('/signup',signup)
 router.post('/signin',signin)
-router.post('/refreshToken',GetNewAccessToken)
-router.delete('/logout',logout)
+router.post('/refreshToken',authorization,GetNewAccessToken)
+router.delete('/logout',authorization,logout)
 router.delete('/deleteSite',authorization,deleteSite)
 // router.post('/forgotPassword',forgotPassword)
+router.get('/generateOTP',generateOTP)
+router.post('/verifyOTP',verifyOTP)
 router.post('/home/addSite',authorization,addSite)
 router.post('/home',authorization,home)
 router.get('/home',authorization,search)
 router.post('/home/selectedSite',authorization,selectedSite)
 router.post('/home/selectedSite/editSite',authorization,editSite)
 router.get('/sync',authorization,home) // get all the site documents of the user
+router.post('/resetMPin',authorization,resetMPin)
 
-module.exports=router
+module.exports=router 
