4d14a53acfdc3ad0a2a7005e4e08e3558e0c8ab7
unknown <sandesh.prabhu@robosoftin.com>
2022-10-25 16:34:48 +0530

    forgot password

--- controller/signinController.js
+++ controller/signinController.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcrypt')
 const userModel=require('../models/user')
 const jwt = require('jsonwebtoken');
+const speakeasy = require('speakeasy')
 const generateTokens = require("../utils/generateTokens");
 // const client=require('twilio')(/*accountSid*/"accountSid",/*authToken*/"authToken")
 require('dotenv').config()
@@ -12,7 +13,7 @@ async function signin(req, res) {
   try {
     if(await bcrypt.compare(req.body.MPin.toString(), user.MPin)) { //compare MPin 
     const tokens =await generateTokens(user) // function call to generate tokens 
-		res.status(200).send({ token: tokens, message: "logged in successfully" }); 
+		res.status(200).send({ accessToken: tokens.accessToken, message: "logged in successfully" }); 
     } else {
       res.status(401).send('Not Allowed')
     }
@@ -20,26 +21,42 @@ async function signin(req, res) {
     res.status(500).send()
   }
 }
-/*
-async function forgotPassword(req, res) {
-  const [user] = await userModel.find({mobileNumber: req.body.mobileNumber}).clone()
-  if (user == null)
-    return res.status(404).send('Cannot find user')
-  const mobileNumber=req.body.mobileNumber
-  const OTP=Math.random() * (9999 - 1000) + 1000
-  try{
-     await client.messages.create({
-      body: OTP, 
-      from: '+15017122661', 
-      to: mobileNumber.toString()
-    })
-  }
-  catch{error=> res.sendStatus(500).send(error)};
-  //compareOTP()
-  // if wrong otp return invalid
-  //else
-  //proceed to login
-}
-*/
 
-module.exports={signin/*,forgotPassword*/}
+async function generateOTP(req, res) {
+ try {
+  const secret = speakeasy.generateSecret({length: 10})
+  res.send({
+    "token": speakeasy.totp({
+        secret: secret.base32,
+        encoding: "base32",
+        step: 30
+    }),
+    "secret":secret.base32,
+    "secretData":secret
+})
+ } catch (error) {
+  console.log(error)
+  res.status(500).send()
+
+ } 
+ 
+}
+async function verifyOTP(req, res) {
+ try {
+  res.send({
+    "valid": speakeasy.totp.verify({
+      secret: req.body.secret,
+      encoding: "base32",
+      token: req.body.token,
+      window: 0,
+      step: 30
+  })
+})
+ } catch (error) {
+  console.log(error)
+  res.status(500).send()
+
+ } 
+}
+
+module.exports={signin,generateOTP,verifyOTP}
