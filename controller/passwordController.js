92e14286662762aa3dc77658d7792b3d8b96332d
unknown <sandesh.prabhu@robosoftin.com>
2022-10-28 12:06:25 +0530

    bug fixed for unregistered user

--- controller/passwordController.js
+++ controller/passwordController.js
@@ -6,8 +6,6 @@ async function resetMPin(req,res){
     if(typeof req.body.oldMPin!="number") return res.send("Enter a valid 4-digit old MPin")
     if(typeof req.body.newMPin!="number") return res.send("Enter a valid 4-digit new MPin")
     if(req.body.oldMPin==req.body.newMPin) return res.send("New MPin cannot be same as old MPin")
-    // const newMPin = req.body.newMPin.toString()
-    // const oldMPin = req.body.oldMPin.toString()
     if(req.body.newMPin.toString().length!=4) return res.send("Enter a valid 4-digit new MPin")
     if(req.body.oldMPin.toString().length!=4) return res.send("Enter a valid 4-digit old MPin")
     const [userData] = await userModel.find({mobileNumber: req.user.mobileNumber}).clone()
@@ -35,10 +33,11 @@ async function addNewMPin(req,res){
     try {
         const salt = await bcrypt.genSalt(Number(process.env.SALT)); // salt generation
         const hashedPassword = await bcrypt.hash(req.body.newMPin.toString(), salt) // bcrypting MPin
-        await userModel.findOneAndUpdate({ mobileNumber: req.body.mobileNumber},{ MPin: hashedPassword },(err)=>{ //Update in database
+        const docs = await userModel.findOneAndUpdate({ mobileNumber: req.body.mobileNumber},{ MPin: hashedPassword },(err)=>{ //Update in database
             if(err) return res.send(err)
         }).clone();
-        res.status(200).send("MPin successfully Updated")
+        if(docs==null) return res.send("User is not registered")
+        return res.status(200).send("MPin successfully Updated")
     } 
     catch(err) {
         return res.status(500).send(err)
